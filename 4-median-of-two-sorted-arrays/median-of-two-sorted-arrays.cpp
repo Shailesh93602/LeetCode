695df32d84f84198693de2a3b9e51be65d3c1540
Shaileshbhai Chaudhari <shailesh93602@gmail.com>
2024-01-21 01:47:57 -0800

    Time: 24 ms (69.95%) | Memory: 95.2 MB (5.19%) - LeetSync

--- 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.cpp
+++ 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.cpp
@@ -7,26 +7,12 @@ public:
         int i=0;
         int j=0;
         while(i<n && j<m) {
-            if(nums1[i] < nums2[j]){
-                nums.push_back(nums1[i]);
-                i++;
-            }
-            else {
-                nums.push_back(nums2[j]);
-                j++;
-            }
+            if(nums1[i] < nums2[j]) nums.push_back(nums1[i++]);
+            else nums.push_back(nums2[j++]);
         }
-        while(i < n) {
-            nums.push_back(nums1[i]);
-            i++;
-        }
-        while(j < m) {
-            nums.push_back(nums2[j]);
-            j++;
-        }
-        if((n+m)%2 != 0) {
-            return nums[(n+m)/2];
-        }
+        while(i < n) nums.push_back(nums1[i++]);
+        while(j < m) nums.push_back(nums2[j++]);
+        if((n+m)%2 != 0) return nums[(n+m)/2];
         double val = nums[(n+m)/2];
         return (val+nums[((n+m)/2)-1])/2;
     }
