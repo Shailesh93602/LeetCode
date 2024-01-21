cd971eafc0c9649e057ec9af635a46810e10336e
Shaileshbhai Chaudhari <shailesh93602@gmail.com>
2024-01-21 07:20:31 -0800

    Time: 11 ms (17.27%) | Memory: 16 MB (6.97%) - LeetSync

--- 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -1,13 +1,34 @@
 class Solution {
 public:
-    vector<int> searchRange(vector<int>& nums, int target) {
-        int first=-1;
-        int last=-1;
-        int n = nums.size();
-        for(int i=0; i<n; i++) {
-            if(nums[i] == target && first == -1) first = i;
-            if(nums[n-i-1] == target && last == -1) last = n-i-1;
+    int binarySearch(vector<int>& nums, int first, int last, int target, bool findFirst) {
+        while (first <= last) {
+            int mid = first + (last - first) / 2;
+            if (nums[mid] == target) {
+                if (findFirst) {
+                    if (mid == 0 || nums[mid - 1] != target) {
+                        return mid;
+                    } else {
+                        last = mid - 1;
+                    }
+                } else {
+                    if (mid == nums.size() - 1 || nums[mid + 1] != target) {
+                        return mid;
+                    } else {
+                        first = mid + 1;
+                    }
+                }
+            } else if (nums[mid] > target) {
+                last = mid - 1;
+            } else {
+                first = mid + 1;
+            }
         }
+        return -1;
+    }
+
+    vector<int> searchRange(vector<int>& nums, int target) {
+        int first = binarySearch(nums, 0, nums.size() - 1, target, true);
+        int last = binarySearch(nums, 0, nums.size() - 1, target, false);
         return {first, last};
     }
 };
