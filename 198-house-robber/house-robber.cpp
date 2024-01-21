19649e39ddcc1ac2298b35376a358f3138515e29
Shaileshbhai Chaudhari <shailesh93602@gmail.com>
2024-01-20 23:34:56 -0800

    Time: 0 ms (100.00%) | Memory: 9.9 MB (9.67%) - LeetSync

--- 198-house-robber/house-robber.cpp
+++ 198-house-robber/house-robber.cpp
@@ -5,7 +5,9 @@ public:
         if(n == 0) return 0;
         if(n == 1) return nums[n-1];
         if(dp.find(n) != dp.end()) return dp[n];
-        return dp[n] = max(nums[n-1] + findMax(nums, n-2), findMax(nums, n-1));
+        int maxAmount = max(nums[n-1] + findMax(nums, n-2), findMax(nums, n-1));
+        dp[n] = maxAmount;
+        return maxAmount;
     }
     int rob(vector<int>& nums) {
         return findMax(nums, nums.size());
