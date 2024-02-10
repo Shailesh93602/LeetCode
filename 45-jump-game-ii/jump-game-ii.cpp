f4e5130fc55b8c3299df162f01f592f02cb18ea5
Shaileshbhai Chaudhari <shailesh93602@gmail.com>
2024-02-09 22:19:56 -0800

    Time: 8 ms (88.09%) | Memory: 19 MB (14.86%) - LeetSync

--- 45-jump-game-ii/jump-game-ii.cpp
+++ 45-jump-game-ii/jump-game-ii.cpp
@@ -1,24 +1,18 @@
 class Solution {
 public:
-    int findRecursive(vector<int> arr, int n) {
-        int val = -1;
-        for(int i=0; i<n; i++) {
-            if(arr[i]+i >=n) {
-                val = i;
-                break;
+    int jump(vector<int>& nums) {
+        int count = 1;
+        int step = nums[0];
+        int maxPos = nums[0];
+        for(int i=1; i<nums.size()-1; i++) {
+            maxPos = max(maxPos, nums[i]+i);
+            step--;
+            if(step == 0){
+                count++;
+                step = maxPos-i;
             }
         }
-        return val;
-    }
-    int jump(vector<int>& nums) {
         if(nums.size() == 1) return 0;
-        int count = 1;
-        int val = findRecursive(nums, nums.size()-1);
-        while(val > 0) {
-            count++;
-            val = findRecursive(nums, val);
-        }
-        if(val == -1) return val;
         return count;
     }
 };
