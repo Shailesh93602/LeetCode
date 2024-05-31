32a439cb02bfdcea294c400214007a2a110466b9
Shaileshbhai Chaudhari <shailesh93602@gmail.com>
2024-05-30 18:15:51 -0700

    Time: 7 ms (62.82%) | Memory: 12.2 MB (87.16%) - LeetSync

--- 260-single-number-iii/single-number-iii.cpp
+++ 260-single-number-iii/single-number-iii.cpp
@@ -1,18 +1,22 @@
 class Solution {
 public:
+    int getBit(int x, int k) {
+        return (x >> k) & 1;
+    }
     vector<int> singleNumber(vector<int>& nums) {
-        if(nums.size() == 2) return nums;
-        unordered_map<int, int> map;
+        int xorXY = 0;
+        for (int num : nums)
+            xorXY ^= num;
 
-        for(int i=0; i<nums.size(); i++) {
-            map[nums[i]]++;
-        }
-        
-        vector<int> ans;
-        for(auto it=map.begin(); it!= map.end(); it++) {
-            if(it->second == 1) ans.push_back(it->first);
-        }
+        int pos = 0;
+        while (!getBit(xorXY, pos)) pos++;
 
-        return ans;
+        int x = 0;
+        for (int num : nums)
+            if (getBit(num, pos))
+                x ^= num;
+
+        int y = xorXY ^ x;
+        return {x, y};
     }
 };
