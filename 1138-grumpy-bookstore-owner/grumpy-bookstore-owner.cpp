f00efbdc6b2cdccc6ca200d68fbadbf2493adfdb
Shaileshbhai Chaudhari <shailesh93602@gmail.com>
2024-06-21 07:38:36 +0530

    Time: 27 ms (40.73%) | Memory: 34.4 MB (16.52%) - LeetSync

--- 1138-grumpy-bookstore-owner/grumpy-bookstore-owner.cpp
+++ 1138-grumpy-bookstore-owner/grumpy-bookstore-owner.cpp
@@ -1,30 +1,22 @@
 class Solution {
 public:
     int maxSatisfied(vector<int>& customers, vector<int>& grumpy, int minutes) {
-        int n = customers.size();
+        int sum = 0;
+        int start = 0;
         int ans = 0;
-        int i;
         
-        vector<int> p(n, 0);
-        vector<vector<int>> dp(n, {2, 0});
-        
-        int k = customers[0];
-        p[0] = grumpy[0] * customers[0]; 
-        dp[0][0] = p[0]; 
-
-        for (i = 1; i < n; i++) {
-            k += customers[i]; 
-            if (grumpy[i] == 1) p[i] = customers[i] + p[i - 1];
-
-            p[i] = max(p[i - 1], p[i]);
-            dp[i][0] = p[i];
-
-            if (i >= minutes) {
-                dp[i][1] = dp[i - 1][1] + p[i] - p[i - 1]; 
-                dp[i][1] = min(dp[i][1], dp[i - minutes][0]); 
+        for(int i=0; i<customers.size(); i++) {
+            if(grumpy[i] == 1) sum += customers[i];
+            if(i-start+1 == minutes) {
+                ans = max(sum, ans);
+                if(grumpy[start] == 1) sum -= customers[start];
+                start++;
             }
         }
-
-        return k - dp[n - 1][1];
+        
+        for(int i=0; i<customers.size(); i++) {
+            if(grumpy[i] == 0) ans += customers[i];
+        }
+        return ans;
     }
 };
