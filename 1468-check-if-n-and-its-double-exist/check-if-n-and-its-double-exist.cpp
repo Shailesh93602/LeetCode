a8e6162d6fba1a223411d4ef9782d903ae112d3a
Shaileshbhai Chaudhari <shailesh93602@gmail.com>
2024-12-01 10:10:15 +0530

    Time: 3 ms (24.95%) | Memory: 14 MB (15.21%) - LeetSync

--- 1468-check-if-n-and-its-double-exist/check-if-n-and-its-double-exist.cpp
+++ 1468-check-if-n-and-its-double-exist/check-if-n-and-its-double-exist.cpp
@@ -1,20 +1,13 @@
 class Solution {
 public:
     bool checkIfExist(vector<int>& arr) {
-        unordered_map<int, int> map;
-        
-        for(int i: arr) map[i]++;
+        unordered_set<int> s;
+
         for(int i: arr) {
-            if(i == 0 && map[0] == 1) continue;
-            if(map.find(i*2) != map.end()) {
-                cout <<map[i*2] <<" ";
+            if(s.count(i*2) || (i%2 == 0 && s.count(i/2))) 
                 return true;
-            }
-            if(i%2 == 0 && map.find(i/2) != map.end()) {
-                cout <<map[i/2];
-                return true;
-            }
-            cout <<endl;
+            
+            s.insert(i);
         }
 
         return false;
