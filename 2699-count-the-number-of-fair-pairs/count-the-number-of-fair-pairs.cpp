da34e16277b9ed3728167386b4578c5551aae3ba
Shaileshbhai Chaudhari <shailesh93602@gmail.com>
2025-04-19 09:17:29 +0530

    Time: 26 ms (91.89%) | Memory: 60.5 MB (7.20%) - LeetSync

--- 2699-count-the-number-of-fair-pairs/count-the-number-of-fair-pairs.cpp
+++ 2699-count-the-number-of-fair-pairs/count-the-number-of-fair-pairs.cpp
@@ -1,23 +1,23 @@
 class Solution {
-public:
     long long solve(vector<int> &nums, int val) {
-        int i=0, j=nums.size()-1;
+        int i = 0, j = nums.size() - 1;
         long long ans = 0;
 
         while(i < j) {
             int sum = nums[i] + nums[j];
 
             if(sum < val) {
-                ans += (j - i);
-                i++;
+                ans += (j - i++);
             }
             else j--;
         }
 
         return ans;
     }
+public:
     long long countFairPairs(vector<int>& nums, int lower, int upper) {
         sort(nums.begin(), nums.end());
-        return solve(nums, upper+1) - solve(nums, lower);
+        
+        return solve(nums, upper + 1) - solve(nums, lower);
     }
 };
