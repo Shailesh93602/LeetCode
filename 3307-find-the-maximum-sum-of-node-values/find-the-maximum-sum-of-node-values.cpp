868400336b9890d566f1cf98187f45fabccb0183
Shaileshbhai Chaudhari <shailesh93602@gmail.com>
2025-05-23 08:30:54 +0530

    Time: 0 ms (100.00%) | Memory: 126.5 MB (87.74%) - LeetSync

--- 3307-find-the-maximum-sum-of-node-values/find-the-maximum-sum-of-node-values.cpp
+++ 3307-find-the-maximum-sum-of-node-values/find-the-maximum-sum-of-node-values.cpp
@@ -1,24 +1,24 @@
 class Solution {
 public:
-    long long maximumValueSum(vector<int>& nums, int k,
-                              vector<vector<int>>& edges) {
-        vector<vector<long long>> memo(nums.size(), vector<long long>(2, -1));
-        return maxSumOfNodes(0, 1, nums, k, memo);
-    }
+    long long maximumValueSum(vector<int>& nums, int k, vector<vector<int>>& edges) {
+        long long ans = 0;
+        int count = 0, minVal = (1 << 30), maxVal = -1 * (1 << 30);
 
-private:
-    long long maxSumOfNodes(int index, int isEven, vector<int>& nums, int k, vector<vector<long long>>& memo) {
-        if (index == nums.size()) {
-            return isEven == 1 ? 0 : INT_MIN;
-        }
-        if (memo[index][isEven] != -1) {
-            return memo[index][isEven];
-        }
-        long long noXorDone =
-            nums[index] + maxSumOfNodes(index + 1, isEven, nums, k, memo);
-        long long xorDone = (nums[index] ^ k) +
-                            maxSumOfNodes(index + 1, isEven ^ 1, nums, k, memo);
+        for(int i: nums) {
+            int val = i ^ k;
+            ans += i;
+            int change = val - i;
 
-        return memo[index][isEven] = max(xorDone, noXorDone);
+            if(change > 0) {
+                minVal = min(minVal, change);
+                ans += change;
+                count++;
+            }
+            else maxVal = max(maxVal, change);
+        }
+
+        if(count % 2 == 0) return ans;
+
+        return max(ans - minVal, ans += maxVal);
     }
 };
