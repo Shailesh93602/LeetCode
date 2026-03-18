37d8b366be294be499f8447cc2ef16dfa9aa4bf7
Shaileshbhai Chaudhari <shailesh93602@gmail.com>
2026-03-18 23:28:47 +0530

    Time: 0 ms (100.00%) | Memory: 20.5 MB (61.63%) - LeetSync

--- 45-jump-game-ii/jump-game-ii.cpp
+++ 45-jump-game-ii/jump-game-ii.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     int jump(vector<int>& nums) {
-        int ans = 0, maxVal, currVal;
+        int ans = 0, maxVal = 0, currVal = 0;
 
         for(int i=0; i<nums.size()-1; i++) {
             maxVal = max(maxVal, (i + nums[i]));
