a542bff6c9cdbc3daeff3b93dfe9e08fff248e3a
Shaileshbhai Chaudhari <shailesh93602@gmail.com>
2026-03-18 09:18:43 +0530

    Time: 0 ms (100.00%) | Memory: 9.8 MB (23.47%) - LeetSync

--- 242-valid-anagram/valid-anagram.cpp
+++ 242-valid-anagram/valid-anagram.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
+        if(s.size() != t.size()) return false;
         vector<int> freq(26, 0);
 
         for(char ch: s) freq[ch-'a']++;
