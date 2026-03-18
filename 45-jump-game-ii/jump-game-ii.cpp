4f7442a74fa11ec78686c342506d428bfddf8f3c
Shaileshbhai Chaudhari <shailesh93602@gmail.com>
2026-03-18 23:27:28 +0530

    Time: 0 ms (100.00%) | Memory: 20.5 MB (32.79%) - LeetSync

--- 45-jump-game-ii/jump-game-ii.cpp
+++ 45-jump-game-ii/jump-game-ii.cpp
@@ -1,18 +1,17 @@
 class Solution {
 public:
     int jump(vector<int>& nums) {
-        int count = 1;
-        int step = nums[0];
-        int maxPos = nums[0];
-        for(int i=1; i<nums.size()-1; i++) {
-            maxPos = max(maxPos, nums[i]+i);
-            step--;
-            if(step == 0){
-                count++;
-                step = maxPos-i;
+        int ans = 0, maxVal, currVal;
+
+        for(int i=0; i<nums.size()-1; i++) {
+            maxVal = max(maxVal, (i + nums[i]));
+
+            if(i == currVal) {
+                ans++;
+                currVal = maxVal;
             }
         }
-        if(nums.size() == 1) return 0;
-        return count;
+
+        return ans;
     }
 };
