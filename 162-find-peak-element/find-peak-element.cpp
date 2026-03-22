ac3026186a2494706e8daaafd097408ad4c71dbe
Shaileshbhai Chaudhari <shailesh93602@gmail.com>
2026-03-22 11:54:40 +0530

    Time: 0 ms (100.00%) | Memory: 12.8 MB (5.35%) - LeetSync

--- 162-find-peak-element/find-peak-element.cpp
+++ 162-find-peak-element/find-peak-element.cpp
@@ -6,8 +6,8 @@ public:
         while(left < right) {
             int mid = left + (right - left) / 2;
 
-            if(nums[mid] < nums[mid + 1]) left = mid + 1;
-            else right = mid; 
+            if(nums[mid] > nums[mid + 1]) right = mid;
+            else left = mid + 1;
         }
 
         return left;
